2ed559e3cf01a5a4836e4a0c9b6d7e06bb43f84a
Daniel Rojas <drojasca@ualberta.ca>
2020-10-01 18:04:29 -0600

    made changes :(

--- app/src/main/java/com/example/cmput301f20t24/Parallelogram.java
+++ app/src/main/java/com/example/cmput301f20t24/Parallelogram.java
@@ -1,7 +1,7 @@
 package com.example.cmput301f20t24;
 
-public class Circle extends Shape {
-    public Circle (int x, int y) {
+public class Parallelogram extends Shape {
+    public Parallelogram (int x, int y) {
         this.x = x;
         this.y = y;
     }
